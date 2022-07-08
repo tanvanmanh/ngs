97db2d2867d1f5dc25ea7673a1b8e609f42976af
manh.tan <manh.tan@mservice.com.vn>
2022-07-08 13:58:49 +0700

    Timers module: Resolve challenges 2

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -1,5 +1,20 @@
 // Print "Hello World"
 // Every second
 // And stop after 5 times
+let times = 1;
+function helloWorld() {
+    console.log('Hello World ' + times);
+    times++;
+
+    if (times > 5) {
+        console.log("Done");
+        clearInterval(this);
+    }
+}
+
+setInterval(
+    helloWorld, 1000
+
+);
 
 // After 5 times. Print "Done" and let Node exit.
